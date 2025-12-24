cad878d80f8f2e129ba9a943c2028b6fa47bdaa4
AkishimaShino <engjo.com@gmail.com>
2025-12-24 21:55:15 +0100

    Update fonction_comptage.h

--- fonction_comptage.h
+++ fonction_comptage.h
@@ -1,5 +1,11 @@
 typedef struct MotNode {
-    char *mot;
+    char * mot;
     size_t compteur;
     struct MotNode *suivant;
-} MotNode;
+} MotNode, *Liste;
+
+
+int recherche (Liste * m, char * mot);
+int creation(Liste *m, char *mot);
+void fusion(Liste *un, Liste *deux);
+void triFusion(Liste *l);
