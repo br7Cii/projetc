a2ac9fc8357434150e723296017fec61aadeba90
AkishimaShino <engjo.com@gmail.com>
2025-12-24 23:18:30 +0100

    Update and rename fonction_comptage.h to algo1.h

---
+++ algo1.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "fonction_allocation.h"
+
+typedef struct MotNode {
+    char *mot;
+    int compteur;
+    struct MotNode *suivant;
+} MotNode, *Liste;
+
+int recherche(Liste *m, char *mot);
+int creation(Liste *m, char *mot, InfoMem *info);
+void Conversion(char *str);
+void MotFichier(FILE *Fichier, Liste *l, InfoMem *info);
+void AfficherResultat(Liste l, int n);
+void libererListe(Liste *m, InfoMem *info);
+void fusion(Liste *un, Liste *deux);
+void triFusion(Liste *l);
+void ecrireFichier(Liste m, int n, const char *nomFichier);
+void ecrirePerformances(InfoMem *info, double temps, const char *fichier);
+void algo1(const char *fichierEntree, int n, InfoMem *info, const char *fichierSortie, int afficher);
