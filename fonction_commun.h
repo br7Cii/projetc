86ddc48d71db68acec32cf18598b8e823c178db9
AkishimaShino <engjo.com@gmail.com>
2025-12-27 16:50:40 +0100

    Update fonction_commun.h

--- fonction_commun.h
+++ fonction_commun.h
@@ -1,10 +1,7 @@
-#ifndef FONCTION_COMMUN_H
-#define FONCTION_COMMUN_H
+#include <stdio.h>
 
 typedef struct MotNode {
     char *mot;
     int compteur;
     struct MotNode *suivant;
 } MotNode, *Liste;
-
-#endif 
