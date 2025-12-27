9ff847c8c49a19d5e156138a4929fe4303f31b1c
AkishimaShino <engjo.com@gmail.com>
2025-12-27 16:47:06 +0100

    Create algo3.h

---
+++ algo3.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "fonction_allocation.h"
+#include "fonction_commun.h"
+
+Liste * dichotomie(Liste *m, char *mot, int len);
+int creation3(Liste *m, char *mot, InfoMem *info);
+void Conversion3(char *str);
+void MotFichier3(FILE *Fichier, Liste *l, InfoMem *info);
+void AfficherResultat3(Liste l, int n);
+void libererListe3(Liste *m, InfoMem *info);
+void fusion3(Liste *un, Liste *deux);
+void triFusion3(Liste *l);
+void ecrireFichier3(Liste m, int n, const char *nomFichier);
+void ecrirePerformances3(InfoMem *info, double temps, const char *fichier);
+void algo3(const char *fichierEntree, int n, InfoMem *info, const char *fichierSortie, int afficher);
